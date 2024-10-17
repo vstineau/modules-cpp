ea94b6636ec0a213b7fb1d606d7a8470e48b764b
Vincent Stineau <vstineau@made-f0Cr2s6.clusters.42paris.fr>
2024-10-17 15:25:14 +0200

    cpp05 ex00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,37 @@
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+
+public:
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	Bureaucrat(std::string name, unsigned int grade);
+	Bureaucrat(Bureaucrat const & src);
+	~Bureaucrat(void);
+	Bureaucrat &operator=(Bureaucrat const & src);
+	std::string const getName(void) const;
+	unsigned int getGrade(void) const;
+	void upgrade();
+	void downgrade();
+
+private:
+	Bureaucrat(void);
+	std::string const _name;
+	unsigned int _grade;
+};
+
+std::ostream & operator<<(std::ostream & o, Bureaucrat const & i);
+
+#endif
