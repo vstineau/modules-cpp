3a5547e81a8ee01329bfaf87edb68482f1d0344a
vstineau <vstineau@student.42.fr>
2024-10-26 23:10:16 +0200

    ex00 cpp6 finit

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -24,20 +24,26 @@ ScalarConverter& ScalarConverter::operator=(ScalarConverter const & src)
 
 bool is_only_one_char(std::string s)
 {
-	char c = std::atoi(s.c_str());
-	if (s[0] >= 32 && s[0] <= 126 && !s[1])
+	long double j = s[0];
+	//std::cout << j << "\n";
+
+	if (j >= 32 && j <= 126 && !s[1])
 		return (true);
 	return (false);
 }
 
 void ScalarConverter::convert(std::string n)
 {
+	int k = 0;
 	long double nb = strtold(n.c_str(), NULL);
-	char c = n[0];
+	while (n[k] == '0')
+		k++;
+	char c = n[k];
+	long double i = n[0];
 	
 	if (nb < std::numeric_limits<char>::min() || nb > std::numeric_limits<char>::max() || std::isnan(nb))
 		std ::cout << "char : impossible\n";
-	else if ((!std::isprint(nb) && !is_only_one_char(n)) || !(nb >= 32 && nb <= 126))
+	else if (!std::isprint(c))//!is_only_one_char(n) || !(i >= 32 && i <= 126))
 		std ::cout << "char : character non displayable\n";
 	else if (is_only_one_char(n))
 		std::cout << "char : " << static_cast<char>(c) << std::endl;
@@ -46,17 +52,17 @@ void ScalarConverter::convert(std::string n)
 	if ((nb < std::numeric_limits<int>::min() || nb > std::numeric_limits<int>::max() || std::isnan(nb)) && !is_only_one_char(n))
 		std ::cout << "int : impossible\n";
 	else if (is_only_one_char(n))
-		std::cout << "int : " << static_cast<int>(c) << std::endl;
+		std::cout << "int : " << static_cast<int>(i) << std::endl;
 	else
 		std::cout << "int : " << static_cast<int>(nb) << std::endl;
 	if ((std::abs(nb) < std::numeric_limits<float>::min() || std::abs(nb) > std::numeric_limits<float>::max()) && !is_only_one_char(n))
 		std ::cout << "float : impossible\n";
 	else if (is_only_one_char(n))
-		std::cout << "float : " << static_cast<float>(c) << std::endl;
+		std::cout << "float : " << static_cast<float>(i) << std::endl;
 	else
 		std::cout << "float : " << static_cast<float>(nb) << "f" << std::endl;
 	if (is_only_one_char(n))
-		std::cout << "float : " << static_cast<float>(c) << std::endl;
+		std::cout << "float : " << static_cast<float>(i) << std::endl;
 	else
 		std::cout << "double : " << static_cast<double>(nb) << std::endl;
 }
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -2,30 +2,38 @@
 #include "ScalarConverter.hpp"
 #include <iostream>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	std::string nb = "42.01234f";
-	std::string nb0 = "A";
-	std::string nb1 = "0";
-	std::string nb2 = "nan";
-	std::string nb3 = "-inf";
-	std::string nb4 = "+inf";
-	std::string nb5 = "-12982938";
-	std::string nb6 = "-12";
-
-	ScalarConverter::convert(nb);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb0);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb1);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb2);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb3);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb4);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb5);
-	std::cout << std::endl;
-	ScalarConverter::convert(nb6);
+	if (argc != 2)
+	{
+		std::cout << "wrong number of arguments\n";
+		return 1;
+	}
+	std::string arg = argv[1];
+	ScalarConverter::convert(arg);
 }
+//	std::string nb = "42.01234f";
+//	std::string nb0 = "A";
+//	std::string nb1 = "0";
+//	std::string nb2 = "nan";
+//	std::string nb3 = "-inf";
+//	std::string nb4 = "+inf";
+//	std::string nb5 = "-12982938";
+//	std::string nb6 = "-12";
+//
+//	ScalarConverter::convert(nb);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb0);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb1);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb2);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb3);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb4);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb5);
+//	std::cout << std::endl;
+//	ScalarConverter::convert(nb6);
+//}
