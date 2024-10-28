debb0fec1b77e34522d49e288fdbc4850b1d88e2
vstineau <vstineau@student.42.fr>
2024-10-28 11:57:04 +0100

    cpp06

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -9,14 +9,11 @@ int main(void)
 
 	ptr->data = "truc";
 	std::cout << "string in ptr is : " << ptr->data << std::endl;
-	std::cout << "adress of ptr is : " << &ptr << std::endl;
 	uintptr_t uintptr = Serializer::serialize(ptr);
 	std::cout << "\n------------------------------------------------\n";
 	std::cout << "value of uintptr is : " << uintptr << std::endl;
-	std::cout << "adress of uintptr is : " << &uintptr << std::endl;
 	std::cout << "\n------------------------------------------------\n";
 	ptr2 = Serializer::deSerialize(uintptr);
 	std::cout << "string in ptr2 is : " << ptr2->data << std::endl;
-	std::cout << "adress of ptr2 is : " << &ptr2 << std::endl;
 	delete ptr;
 }
