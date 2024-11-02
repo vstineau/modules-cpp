9774ee00af15216463c231297d29ca46e1c8057d
vstineau <vstineau@student.42.fr>
2024-11-02 17:33:05 +0100

    save

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -4,12 +4,39 @@
 Span::Span()
 {}
 
+Span::Span(unsigned int n): _n(n)
+{}
+
 Span::~Span()
 {}
 
-Span::Span(Span const & src)
+Span::Span(Span const & src): _vecti(src._vecti), _n(src._n)
 {}
 
 Span& Span::operator=(Span const & src)
-{}
+{
+	if (this != &src)
+	{
+		_vecti = src._vecti;
+		_n = src._n;
+	}
+	return (*this);
+}
 
+void Span::addNumber(int newNb)
+{
+	if (_vecti.size() < _n)
+		_vecti.push_back(newNb);
+	else
+		throw NoMoreSPaceException();
+}
+
+const char *Span::NoMoreSPaceException::what() const throw()
+{
+	return ("no more space in span, can't add another integrer\n");
+}
+
+const char *Span::NoEnoughNumberException::what() const throw()
+{
+	return ("not enough number to answer\n");
+}
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -2,15 +2,34 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+#include <algorithm>
+#include <exception>
+
 class Span {
 
 public:
-	Span();
+	class NoMoreSPaceException : std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class NoEnoughNumberException : std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	Span(unsigned int n);
 	Span(Span const & src);
 	~Span();
 	Span &operator=(Span const & src);
+	void addNumber(int newNb);
+	int shortestSpan();
+	int longestSpan();
 
 private:
+	Span();
+	std::vector<int> _vecti;
+	unsigned int _n;
 };
 
 #endif
